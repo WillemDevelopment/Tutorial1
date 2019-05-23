9a65f752c83940974629b213fc59df735eac724c
StephanBH <bghdpayments@gmail.com>
2019-05-22 18:05:12 -0700

    Added a jumpboost event

--- src/main/java/net/noodles/tutorial1/main/events/Events.java
+++ src/main/java/net/noodles/tutorial1/main/events/Events.java
@@ -1,5 +1,8 @@
 package net.noodles.tutorial1.main.events;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,6 +10,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Events implements Listener {
 
@@ -33,6 +39,33 @@ public class Events implements Listener {
             }
         }
     }
+
+
+
+
+    @EventHandler
+    public void onFall(EntityDamageEvent e) {
+        if(e.getCause() == EntityDamageEvent.DamageCause.FALL) {
+            e.setCancelled(true);
+        }
+    }
+
+
+    @EventHandler
+    public void onPlayerEmeraldJump(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        Location l = p.getLocation();
+        l.add(0,-1,0);
+        Block b = l.getBlock();
+        if(b.getType() == (Material.EMERALD_BLOCK)) {
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 15, 9));
+        } else {
+            return;
+        }
+    }
+
+
+
 
 
 }
