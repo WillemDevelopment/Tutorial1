7e05f4c7b000c3edd87de31f9b871266c2fb1a5d
StephanBH <bghdpayments@gmail.com>
2019-08-17 20:00:54 -0700

    Added ACtionBars

--- src/main/java/net/noodles/tutorial1/main/events/BossBarJoinEvent.java
+++ src/main/java/net/noodles/tutorial1/main/events/BossBarJoinEvent.java
@@ -1,5 +1,6 @@
 package net.noodles.tutorial1.main.events;
 
+import com.connorlinfoot.actionbarapi.ActionBarAPI;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -12,9 +13,12 @@ import org.inventivetalent.bossbar.BossBarAPI;
 public class BossBarJoinEvent implements Listener {
 
     @EventHandler
-    public void onbossbarJoin(PlayerJoinEvent e) {
+    public void onNotficationJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         BossBar bossBar = BossBarAPI.addBar(p,new TextComponent(ChatColor.RED + "Hello " + ChatColor.GREEN + p.getName() + ChatColor.RED + "!"), BossBarAPI.Color.BLUE, BossBarAPI.Style.NOTCHED_20, 1.0f, 0, 0);
+        ActionBarAPI.sendActionBar(p, ChatColor.RED + "This is an actionbar located above your inventory! Welcome " + ChatColor.YELLOW + p.getName() + ChatColor.RED + "!");
+
+
     }
 
 }
