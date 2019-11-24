4a1cfbc891b5f0a53b9d5c4aeb38a33ba5adc2a1
StephanBH <bghdpayments@gmail.com>
2019-11-24 14:23:44 -0800

    Added/Recoded Teleport Command

--- src/main/java/net/noodles/tutorial1/main/commands/TPCommand.java
+++ src/main/java/net/noodles/tutorial1/main/commands/TPCommand.java
@@ -3,11 +3,13 @@ package net.noodles.tutorial1.main.commands;
 import net.noodles.tutorial1.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import sun.java2d.windows.GDIRenderer;
 
 public class TPCommand implements CommandExecutor {
 
@@ -24,17 +26,71 @@ public class TPCommand implements CommandExecutor {
                 return true;
             }
             if(args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Correct usage: /tp <player>");
+                player.sendMessage(ChatColor.RED + "Correct usage: /tp <player> <otherplayer> or /tp <x> <y> <z> <player>");
                 return true;
             }
-            Player target = Bukkit.getPlayer(args[0]);
-            if(target == null) {
-                player.sendMessage(ChatColor.RED + "That player is not online!");
-                return true;
+            if(args.length >=3) {
+                if (args.length == 3) {
+                    int x;
+                    int y;
+                    int z;
+                    try {
+                        x = Integer.parseInt(args[0]);
+                        y = Integer.parseInt(args[1]);
+                        z = Integer.parseInt(args[2]);
+                    } catch (NumberFormatException e) {
+                        player.sendMessage(ChatColor.RED + "Invalid coordinates!");
+                        return true;
+                    }
+                    player.teleport(new Location(player.getWorld(), x, y, z));
+                    player.sendMessage(ChatColor.GREEN + "Teleported to " + x + ", " + y + ", " + z + ".");
+                    return true;
+                }
+                if (args.length == 4) {
+                    int x;
+                    int y;
+                    int z;
+                    try {
+                        x = Integer.parseInt(args[0]);
+                        y = Integer.parseInt(args[1]);
+                        z = Integer.parseInt(args[2]);
+                    } catch (NumberFormatException e) {
+                        player.sendMessage(ChatColor.RED + "Invalid coordinates!");
+                        return true;
+                    }
+                    Player target = Bukkit.getPlayer(args[3]);
+                    if (target == null || !target.isOnline()) {
+                        player.sendMessage(ChatColor.RED + "That player is not online!");
+                        return true;
+                    }
+                    target.teleport(new Location(player.getWorld(), x, y, z));
+                    player.sendMessage(ChatColor.GREEN + "Teleported " + target.getName() + " to " + x + "," + y + "," + z + ".");
+                    target.sendMessage(ChatColor.GREEN + "You were teleported to " + x + "," + y + "," + z);
+                    return true;
+                }
+            } else if (args.length >=1) {
+                Player target = Bukkit.getPlayer(args[0]);
+                    if(target == null || !target.isOnline()) {
+                        player.sendMessage(ChatColor.RED + "That player is not online.");
+                        return true;
+                }
+                    if (args.length == 2) {
+                        Player target2 = Bukkit.getPlayer(args[1]);
+                        if (target2 == null || !target2.isOnline()) {
+                            player.sendMessage(ChatColor.RED + "That player is not online.");
+                            return true;
+                        }
+                        target.teleport(target2);
+                        player.sendMessage(ChatColor.GREEN + "Teleported " + target.getName() +  " to " + target2.getName());
+                        return true;
+                    }
+                    player.teleport(target);
+                    player.sendMessage(ChatColor.GREEN + "Teleported to " + target.getName());
+                    return true;
+            } else {
+                player.sendMessage(ChatColor.RED + "Correct usage: /tp <player> <otherplayer> or /tp <x> <y> <z> <player>");
             }
-            player.teleport(target);
-            player.sendMessage(ChatColor.GREEN + "You teleported to " + target.getName() + "!");
-            target.sendMessage(ChatColor.GREEN + player.getName() + " teleported to you!");
+
         }
         return true;
     }
