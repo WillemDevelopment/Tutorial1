ebb4c376e973edfde95238032e2c1be215bf2932
BGHDDevelopment <dchsbh89@gmail.com>
2020-03-15 13:15:19 -0700

    Updated to latest NPCLib

--- src/main/java/net/noodles/tutorial1/main/NPC/NPCManager.java
+++ src/main/java/net/noodles/tutorial1/main/NPC/NPCManager.java
@@ -2,26 +2,29 @@ package net.noodles.tutorial1.main.NPC;
 
 import net.jitse.npclib.NPCLib;
 import net.jitse.npclib.api.NPC;
-import net.jitse.npclib.events.NPCInteractEvent;
-import net.jitse.npclib.skin.MineSkinFetcher;
+import net.jitse.npclib.api.events.NPCInteractEvent;
+import net.jitse.npclib.api.skin.MineSkinFetcher;
+import net.jitse.npclib.api.state.NPCSlot;
 import net.noodles.tutorial1.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 
 public class NPCManager implements Listener {
 
-
     private Main main;
     private NPCLib npcLib;
 
-    private NPC test;
+    //NPCS HERE
+    private NPC gameLobbyNPC;
 
     public NPCManager(Main main) {
         this.main = main;
@@ -30,37 +33,33 @@ public class NPCManager implements Listener {
         Bukkit.getPluginManager().registerEvents(this, main);
     }
 
-    public void display(Player player) {
-        test.show(player);
-    }
-
 
     private void load() {
-
-        MineSkinFetcher.fetchSkinFromIdAsync(277513, skin -> {
-            test = npcLib.createNPC(skin, Arrays.asList("THIS IS A TEST!", "Click me to send a message"));
-            test.create(new Location(Bukkit.getWorlds().get(0), 0, 55, 0, 50, 0));
-
+        int skinId = 277513;
+        MineSkinFetcher.fetchSkinFromIdAsync(skinId, skin -> {
+            gameLobbyNPC = npcLib.createNPC(Arrays.asList(ChatColor.GOLD + "Game Lobbies", ChatColor.GRAY + "(Click to connect)"));
+            gameLobbyNPC.setLocation(new Location(Bukkit.getWorlds().get(0), 100, 70, 100, 0, 0));
+            gameLobbyNPC.setItem(NPCSlot.MAINHAND, new ItemStack(Material.ANVIL));
+            gameLobbyNPC.setSkin(skin);
+            gameLobbyNPC.create();
         });
-
-
     }
 
-
     @EventHandler
-    public void onNPCInteract(NPCInteractEvent e) {
-        Player player = e.getWhoClicked();
-        if (e.getNPC() == test) {
-            player.sendMessage(ChatColor.GREEN + "You have clicked an NPC!");
+    public void onNPCInteract(NPCInteractEvent event) {
+        Player player = event.getWhoClicked();
+        //DETECT WHEN PLAYERS HIT AN NPC AND DO SOMETHING
+        if(event.getNPC() == gameLobbyNPC) {
+            player.sendMessage(ChatColor.RED + "This is a message when you click me!");
         }
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
-        display(player);
+    public void onJoin(PlayerJoinEvent event) {
+        //SPAWN NPC'S WHEN PLAYER JOINS THE SERVER
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.getPlugin(), () -> {
+            Bukkit.getScheduler().runTask(Main.getPlugin(), () -> gameLobbyNPC.show(event.getPlayer()));
+        }, 60L);
     }
-
-
 
 }
